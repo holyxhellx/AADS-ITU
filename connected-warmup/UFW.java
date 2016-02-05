cc2b1d352ad090b8f3bd028d623a88e9d805ca63
Asger <asgerergod@hotmail.com>
2016-02-05 16:17:07 +0100

    My solution to UFW.java
    uploaded my main method: Lecture 1

--- connected-warmup/UFW.java
+++ connected-warmup/UFW.java
@@ -1,15 +1,21 @@
-import edu.princeton.cs.algs4.*; 
+import edu.princeton.cs.algs4.*;
 
 public class UFW {
-        public static void main(String[] args) {
+
+    public static void main(String[] args) {
         int N = StdIn.readInt();
         WeightedQuickUnionUF uf = new WeightedQuickUnionUF(N);
+        // Changed from QuickUnion
         while (!StdIn.isEmpty()) {
             int p = StdIn.readInt();
             int q = StdIn.readInt();
             if (uf.connected(p, q)) continue;
             uf.union(p, q);
+            //StdOut.println(p + " " + q);
         }
-        StdOut.println(uf.connected(0, 1));
+        if (uf.connected(1, 0)) StdOut.println("TRUE");
+            else StdOut.println("FALSE");
+        //StdOut.println(uf.count() + " components");
     }
+
 }
