7d3b00becd735434614fa28349948564cc92f7f6
Adrian Brink <adrianbrink@users.noreply.github.com>
2016-03-02 19:02:25 +0100

    still not working

--- Congress/Congress.java
+++ Congress/Congress.java
@@ -1,9 +1,8 @@
 import java.util.PriorityQueue;
 import edu.princeton.cs.algs4.*;
+import java.lang.Math.*;
 
 public class Congress {
-
-    public static final int constant = 500000;
 
     public static void main(String[] args) {
         StdOut.println("=====");
@@ -12,24 +11,24 @@ public class Congress {
         int numberOfSeats = StdIn.readInt() - numberOfStates; //4-2
 
         MaxPQ<States> pq = new MaxPQ<States>(numberOfStates, new SeatComparator());
-        for (int i = 0; i < numberOfStates; i++) {
+        for (int i = 1; i <= numberOfStates; i++) {
             String tmpName = StdIn.readString();
-            StdOut.println(i);
             int tmpPopulation = StdIn.readInt();
-            pq.insert(new States(tmpPopulation, tmpName, 1));
+            int pv = (int) ((tmpPopulation) / (Math.sqrt(((1 + 1) * 1)))); 
+            //StdOut.println(tmpName + " " + pv);
+
+            // starts from base 0 and hence the 2nd's seat pv is: n = 1 for the curret seat, and n+1 for the next potential seat
+            pq.insert(new States(tmpName, 1, tmpPopulation, pv));
         } 
 
         for (int i = numberOfSeats; i > 0; i--) {
-            StdOut.println(i);
             States head = pq.delMax();
-            States newHead = null;
-            int newSeatsReceived = head.seatsReceived;
-            int newNumberOfRemainingResidents = head.numberOfRemainingResidents - constant;
-            if (newNumberOfRemainingResidents <= 0) { 
-                newHead = new States(0, head.name, newSeatsReceived);
-            } else {
-                newHead = new States(newNumberOfRemainingResidents, head.name, (newSeatsReceived + 1));
-            }
+            int currentSeatsReceived = head.seatsReceived;
+            int pv = (int) ((head.numberOfTotalResidents) / (Math.sqrt(((currentSeatsReceived + 1) * currentSeatsReceived))));
+
+            States newHead = new States(head.name, (head.seatsReceived + 1), pv);
+            //StdOut.println(newHead.name + " " + newHead.pv);
+
             pq.insert(newHead);
         }
 
@@ -40,3 +39,7 @@ public class Congress {
         StdOut.println("=====");
     }
 }
+
+/* small-4-in is wrong. if you use the actual formula you get 2 and 2
+
+*/
