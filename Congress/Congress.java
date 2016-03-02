800f5c7ce302edae9ea00f8ed97de5b5d369710d
Adrian Brink <adrianbrink@users.noreply.github.com>
2016-03-02 17:43:59 +0100

    Fixed it

--- Congress/Congress.java
+++ Congress/Congress.java
@@ -3,26 +3,35 @@ import edu.princeton.cs.algs4.*;
 
 public class Congress {
 
+    public static final int constant = 500000;
+
     public static void main(String[] args) {
         StdOut.println("=====");
 
         int numberOfStates = StdIn.readInt(); //2
         int numberOfSeats = StdIn.readInt() - numberOfStates; //4-2
 
-        PriorityQueue<States> pq = new PriorityQueue<States>(numberOfStates, new SeatComparator());
+        MaxPQ<States> pq = new MaxPQ<States>(numberOfStates, new SeatComparator());
         for (int i = 0; i < numberOfStates; i++) {
             String tmpName = StdIn.readString();
             StdOut.println(i);
             int tmpPopulation = StdIn.readInt();
-            States state = new States(tmpPopulation, tmpName, 1);
-            pq.add(state);
+            pq.insert(new States(tmpPopulation, tmpName, 1));
         } 
 
         for (int i = numberOfSeats; i > 0; i--) {
-            States head = pq.poll();
-            head.numberOfRemainingResidents -= 10;
-            head.seatsReceived += 1;
-            pq.add(head);
+            StdOut.println(i);
+            States head = null;
+            head = pq.delMax();
+            States newHead = null;
+            int newSeatsReceived = head.seatsReceived;
+            int newNumberOfRemainingResidents = head.numberOfRemainingResidents - constant;
+            if (newNumberOfRemainingResidents <= 0) { 
+                newHead = new States(0, head.name, newSeatsReceived);
+            } else {
+                newHead = new States(newNumberOfRemainingResidents, head.name, (newSeatsReceived + 1));
+            }
+            pq.insert(newHead);
         }
 
         for (States element : pq) {
