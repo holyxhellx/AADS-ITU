5bb7fef8340eccb76d81f43e745e406befd9ddff
Simon  Flachs <Simon@Simons-MacBook-Air-4.local>
2016-03-15 15:52:02 +0100

    implemented sorting with bottom up merge sort. No alterations in the merge sort code

--- 05 Runsort/Runsort.java
+++ 05 Runsort/Runsort.java
@@ -1,39 +1,86 @@
 import edu.princeton.cs.algs4.*;
 
-public class Runsort 
-{
-	public static void main(String[] args)
-	{
-		String[] a = StdIn.readAllStrings();
-		sort(a);
-		StdOut.println("Is sorted: " + isSorted(a));
-		show(a);
-	}
+public class Runsort {
+  
+    private Runsort() { } // This class should not be instantiated.
 
-	public static void sort(Comparable[] a)
-	{
+    // stably merge a[lo..mid] with a[mid+1..hi] using aux[lo..hi]
+    private static void merge(Comparable[] a, Comparable[] aux, int lo, int mid, int hi) {
 
-	}
+        // copy to aux[]
+        for (int k = lo; k <= hi; k++) {
+            aux[k] = a[k]; 
+        }
+
+        // merge back to a[]
+        int i = lo, j = mid+1;
+        for (int k = lo; k <= hi; k++) {
+            if      (i > mid)              a[k] = aux[j++];  // this copying is unneccessary
+            else if (j > hi)               a[k] = aux[i++];
+            else if (less(aux[j], aux[i])) a[k] = aux[j++];
+            else                           a[k] = aux[i++];
+        }
+
+    }
+
+    /**
+     * Rearranges the array in ascending order, using the natural order.
+     * @param a the array to be sorted
+     */
+    public static void sort(Comparable[] a) {
+        int N = a.length;
+        Comparable[] aux = new Comparable[N];
+        for (int n = 1; n < N; n = n+n) {
+            for (int i = 0; i < N-n; i += n+n) {
+                int lo = i;
+                int m  = i+n-1;
+                int hi = Math.min(i+n+n-1, N-1);
+                merge(a, aux, lo, m, hi);
+            }
+        }
+        assert isSorted(a);
+    }
+
+  	/***********************************************************************
+    *  Helper sorting functions.
+    ***************************************************************************/
+    
+    // is v < w ?
+    private static boolean less(Comparable v, Comparable w) {
+        return v.compareTo(w) < 0;
+    }
+
+   // exchange a[i] and a[j]
+    private static void exch(Object[] a, int i, int j) {
+        Object swap = a[i];
+        a[i] = a[j];
+        a[j] = swap;
+    }
 
 
-	public static boolean isSorted(Comparable[] a)
-	{
-		for (int i = 1; i < a.length; i++)
-			if (less(a[i], a[i-1])) return false;
-		return true;
-	}
+   /***************************************************************************
+    *  Check if array is sorted - useful for debugging.
+    ***************************************************************************/
+    private static boolean isSorted(Comparable[] a) {
+        for (int i = 1; i < a.length; i++)
+            if (less(a[i], a[i-1])) return false;
+        return true;
+    }
 
-	public static void show(Comparable[] a)
-	{
-		for (int i = 0; i < a.length; i ++) StdOut.print(a[i] + " ");
-		StdOut.println();
-	}
+    // print array to standard output
+    private static void show(Comparable[] a) {
+        for (int i = 0; i < a.length; i++) {
+            StdOut.println(a[i]);
+        }
+    }
 
-	@SuppressWarnings("unchecked")
-	private static boolean less(Comparable v, Comparable w)
-	{
-		return v.compareTo(w) < 0;
-	}
-
-
+    /**
+     * Reads in a sequence of strings from standard input; bottom-up
+     * mergesorts them; and prints them to standard output in ascending order. 
+     */
+    public static void main(String[] args) {
+        String[] a = StdIn.readAllStrings();
+        Runsort.sort(a);
+        show(a);
+    }
 }
